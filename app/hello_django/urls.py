b5403e87ddb5027d01a38da79102d99b14e6b751
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2025-03-02 23:01:46 +0100

    add home page for testing purposes

--- app/hello_django/urls.py
+++ app/hello_django/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from hello_django.views import home
 from upload.views import image_upload
 
 urlpatterns = [
-    path("", image_upload, name="upload"),
+    path("", home, name="home"),
+    path("upload/", image_upload, name="upload"),
     path("admin/", admin.site.urls),
 ]
 
